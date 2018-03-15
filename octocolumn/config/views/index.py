facfc836a00b955491f4570b40d35f5d90cedd2a
이주현 <>
2018-03-15 11:13:49 +0900

    로그인 부분 이상현상 수정

--- octocolumn/config/views/index.py
+++ octocolumn/config/views/index.py
@@ -15,46 +15,41 @@ __all__ = (
 
 def index(request):
     if request.COOKIES:
-        try:
-            request.COOKIES.get('token')
+        token = request.COOKIES.get('token')
+        if token is not None:
             response = render_to_response("view/main.html", {"login": True})
             return response
-        except ObjectDoesNotExist:
-            return render_to_response('view/main.html', )
+        return render_to_response('view/main.html', )
     return render_to_response('view/main.html',)
 
 
 def write(request):
     if request.COOKIES:
-        try:
-            request.COOKIES.get('token')
+        token = request.COOKIES.get('token')
+        if token is not None:
             response = render_to_response("view/write.html", {"login": True})
             return response
-        except ObjectDoesNotExist:
-            return render_to_response('view/main.html', )
+        return render_to_response('view/main.html', )
     return render_to_response('view/main.html', )
 
 
 def read(request, post_id):
-
     if request.COOKIES:
-        try:
-            request.COOKIES.get('token')
+        token = request.COOKIES.get('token')
+        if token is not None:
             response = render_to_response("view/read.html", {"login": True})
             return response
-        except ObjectDoesNotExist:
-            return render_to_response('view/main.html', )
+        return render_to_response('view/main.html', )
     return render_to_response('view/main.html', )
 
 
 def profile(request):
     if request.COOKIES:
-        try:
-            request.COOKIES.get('token')
+        token = request.COOKIES.get('token')
+        if token is not None:
             response = render_to_response("view/profile.html", {"login": True})
             return response
-        except ObjectDoesNotExist:
-            return render_to_response('view/main.html', )
+        return render_to_response('view/main.html', )
     return render_to_response('view/main.html', )
 
 
