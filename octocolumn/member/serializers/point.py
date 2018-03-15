485594d110b974d8b2f1cedbc12544eb223e0daa
이주현 <>
2018-03-15 14:04:03 +0900

    플러스 마이너스 추가

--- octocolumn/member/serializers/point.py
+++ octocolumn/member/serializers/point.py
@@ -1,11 +1,23 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
 from column.serializers import PostSerializer
 from member.models import PointHistory
 
 
 class PointHistorySerializer(serializers.ModelSerializer):
+    def get_plus_minus(self, obj):
+        if obj.point_use_type == 'Buy':
+            return -1
+        if obj.point_use_type == 'Charge':
+            return 1
+        if obj.point_use_type == 'Reward':
+            return 1
+        if obj.point_use_type == 'Publish':
+            return -1
+
     post = PostSerializer()
+    plus_minus = SerializerMethodField()
 
     class Meta:
         model = PointHistory
