b39491df1288d7fe038cca5af8377cb9f8f7f657
Unknown <jhe0956@gmail.com>
2018-03-18 23:55:55 +0900

    라이트 뒤에 숫자추가 가능하게 변경

--- octocolumn/config/urls/views.py
+++ octocolumn/config/urls/views.py
@@ -9,7 +9,7 @@ from member.apis.verify import VerifyEmail, PasswordResetEmail
 
 urlpatterns = [
     url(r'^$', index, name='index'),
-    url(r'^write/$',write, name='write'),
+    url(r'^write/(?P<temp_id>\d+)$', write, name='write'),
     url(r'^read/(?P<post_id>\d+)$', read, name='read'),
     url(r'^profile/$', profile, name='profile'),
     url(r'^kakao-login/$', kakao, name='kakao-login'),
