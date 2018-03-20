ef6d278e007d401bfa6007cda1149c288c3d88d4
Unknown <jhe0956@gmail.com>
2018-03-20 17:45:48 +0900

    주소를 aws

--- octocolumn/column/apis/post.py
+++ octocolumn/column/apis/post.py
@@ -195,8 +195,8 @@ class PostListView(APIView):
             return ProfileImageSerializer(img).data
         except ObjectDoesNotExist:
             return {
-                "profile_image": '/example/2_x20_.jpeg',
-                "cover_image": '/example/1.jpeg'
+                "profile_image": 'https://devtestserver.s3.amazonaws.com/media/example/2_x20_.jpeg',
+                "cover_image": 'https://devtestserver.s3.amazonaws.com/media/example/1.jpeg'
                     }
 
     def follower_status(self, user):
