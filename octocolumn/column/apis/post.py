58aa8e0eb3ad156fc9fc1fd1a2341a4763c48cbc
이주현 <jhe0956@gmail.com>
2018-03-21 15:59:17 +0900

    유알엘 수정

--- octocolumn/column/apis/post.py
+++ octocolumn/column/apis/post.py
@@ -195,8 +195,8 @@ class PostListView(APIView):
             return ProfileImageSerializer(img).data
         except ObjectDoesNotExist:
             return {
-                "profile_image": 'https://devtestserver.s3.amazonaws.com/media/example/2_x20_.jpeg',
-                "cover_image": 'https://devtestserver.s3.amazonaws.com/media/example/1.jpeg'
+                "profile_image": '/static/example/2_x20_.jpeg',
+                "cover_image": '/static/example/1.jpeg'
                     }
 
     def follower_status(self, user):
