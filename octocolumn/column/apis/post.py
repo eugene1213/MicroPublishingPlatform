aebec759a4a9d8bc74d5af6e758dd1711c5d6270
이주현 <jhe0956@gmail.com>
2018-03-25 23:21:49 +0900

    모델변경

--- octocolumn/column/apis/post.py
+++ octocolumn/column/apis/post.py
@@ -133,6 +133,7 @@ class PostCreateView(generics.GenericAPIView,
                 if self.major_point().point > user_queryset.point:
                     raise exceptions.NotAcceptable({"detail": "There is not enough points."}, 400)
 
+                print(data['preview'])
                 post = Post.objects.create(author=user, title=temp.title,
                                            main_content=temp.main_content,
                                            price=data['price'],
