8727ab978cc9805b91a870a7292eaf3cc31b907c
이주현 <jhe0956@gmail.com>
2018-03-28 12:42:22 +0900

    가즈아

--- octocolumn/member/apis/verify.py
+++ octocolumn/member/apis/verify.py
@@ -1,15 +1,12 @@
 from django.http import HttpResponseRedirect
 from django.utils.encoding import force_text
 from django.utils.http import urlsafe_base64_decode
-from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework_jwt.settings import api_settings
 
 from member.models import User, Profile, ProfileImage
 from member.serializers import UserSerializer
-from utils.jwt import jwt_token_generator
 from utils.tokengenerator import account_activation_token
 
 __all__ = (
@@ -65,24 +62,8 @@ class PasswordResetEmail(APIView):
         except(TypeError, ValueError, OverflowError, User.DoesNotExist):
             user = None
         if user is not None and account_activation_token.check_token(user, token):
+            serializer = UserSerializer(user)
 
-            data = {
-                'token': jwt_token_generator(user),
-                'user': UserSerializer(user).data,
-            }
-
-            if data['user']['is_active']:
-                response = Response(data, status=status.HTTP_200_OK)
-                # response = render_to_response("view/main.html", {"login": True})
-                # response = HttpResponseRedirect(redirect_to='/')
-                if api_settings.JWT_AUTH_COOKIE:
-                    response.set_cookie(api_settings.JWT_AUTH_COOKIE,
-                                        data['token'],
-                                        max_age=21600,
-                                        httponly=True)
-                    self.saved_login_log(user)
-                    return response
-                # return response
-                return response
+            return Response(serializer.data, status=200)
         else:
             return Response('Activation link is invalid!', status=404)
