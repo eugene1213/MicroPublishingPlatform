b9a28ab882bb60cca82ef198dee4ff3da9d8b71a
김유진 <kim-eugene@gim-yujin-ui-MacBook-Pro.local>
2018-04-02 16:34:10 +0900

    no message

--- octocolumn/static/js/signin.js
+++ octocolumn/static/js/signin.js
@@ -45,7 +45,46 @@ function signin_api() {
         error: function(error) {
             if(error.status == 401) {
               // alertBoxFocus('이메일과 비밀번호를 확인해 주시기바랍니다.',401)
-                alert('이메일과 비밀번호를 확인해주세요')
+                modal({
+                    type: 'inverted', //Type of Modal Box (alert | confirm | prompt | success | warning | error | info | inverted | primary)
+                    title: '이메일과 비밀번호를 확인해주세요', //Modal Title
+                    text: '이메일과 비밀번호를 확인해주세요.', //Modal HTML Content
+                    size: 'normal', //Modal Size (normal | large | small)
+                    buttons: [{
+                        text: 'OK', //Button Text
+                        val: 'ok', //Button Value
+                        eKey: true, //Enter Keypress
+                        addClass: 'btn-light-blue', //Button Classes (btn-large | btn-small | btn-green | btn-light-green | btn-purple | btn-orange | btn-pink | btn-turquoise | btn-blue | btn-light-blue | btn-light-red | btn-red | btn-yellow | btn-white | btn-black | btn-rounded | btn-circle | btn-square | btn-disabled)
+                        onClick: function(argument) {
+                            console.log(argument);
+                            alert('Look in console!');
+                        }
+                    }, ],
+                    center: true, //Center Modal Box?
+                    autoclose: false, //Auto Close Modal Box?
+                    callback: null, //Callback Function after close Modal (ex: function(result){alert(result);})
+                    onShow: function(r) {}, //After show Modal function
+                    closeClick: true, //Close Modal on click near the box
+                    closable: true, //If Modal is closable
+                    theme: 'atlant', //Modal Custom Theme
+                    animate: false, //Slide animation
+                    background: 'rgba(0,0,0,0.35)', //Background Color, it can be null
+                    zIndex: 1050, //z-index
+                    buttonText: {
+                        ok: 'OK',
+                        yes: 'Yes',
+                        cancel: 'Cancel'
+                    },
+                    template: '<div class="modal-box"><div class="modal-inner"><div class="modal-title"><a class="modal-close-btn"></a></div><div class="modal-text"></div><div class="modal-buttons"></div></div></div>',
+                    _classes: {
+                        box: '.modal-box',
+                        boxInner: ".modal-inner",
+                        title: '.modal-title',
+                        content: '.modal-text',
+                        buttons: '.modal-buttons',
+                        closebtn: '.modal-close-btn'
+                    }
+                });
             }
         }
     });
