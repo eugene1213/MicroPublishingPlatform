c3da0813022a4ffcf393c8eee1830ca8a9e4b7db
김유진 <kim-eugene@gim-yujin-ui-MacBook-Pro.local>
2018-04-04 14:53:57 +0900

    no message

--- octocolumn/static/js/title-to-header.js
+++ octocolumn/static/js/title-to-header.js
@@ -1,17 +1,3 @@
-function currentPage2header() {     // 현재 페이지 이름을 메뉴 버튼 텍스트에 넣는다.
-
-    var current_url = window.location.href;
-    var currentPage = current_url.split("/");
-    var length = currentPage.length;
-        currentPage = currentPage[length-2];
-
-        
-        if(currentPage != "" && length > 4){
-            currentPage == "write" ? $(".btn-menu > img + div").text("New Story") : $(".btn-menu > img + div").text(currentPage);
-        }else{
-            $(".btn-menu > img + div").text("octocolumn");
-        }
-}
 function title2header(read_or_write){            // 글 읽기,쓰기 페이지에서 제목에 입력된 값이 헤더 로고에 박힌다.
 
     if(read_or_write == "write"){                                 // 글쓰기 모드
