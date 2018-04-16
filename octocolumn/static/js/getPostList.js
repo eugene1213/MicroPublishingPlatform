68662470b8ef8d6b6294920510daedd46989175f
김유진 <kim-eugene@gim-yujin-ui-MacBook-Pro.local>
2018-04-16 19:38:47 +0900

    no message

--- octocolumn/static/js/getPostList.js
+++ octocolumn/static/js/getPostList.js
@@ -8,7 +8,7 @@ function getData(){
     var data = {};
     $.ajax({
         url: "/api/column/postList/",
-        async: false,
+        async: true,
         type: 'GET',
         dataType: 'json',
         success: function(jsons) {
